3066e2e37a15eb153053108ebc5614ebee16ccf9
DianaB177 <anaidb77@gmail.com>
2024-07-15 13:34:36 +0200

    server update

--- src/service/EventService.jsx
+++ src/service/EventService.jsx
@@ -1,25 +1,32 @@
 import axios from 'axios';
 
-
 const baseUrl = process.env.REACT_APP_BACKEND_URL;
 
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    'Content-Type': 'application/json'
+  }
+});
 
 export const getEvento = () => {
-  return axios.get(baseUrl + "/events")
+  return axiosInstance.get("/events");
 }
 
 export const getEventoById = (id) => {
-  return axios.get(baseUrl + "/events/" + id)
+  return axiosInstance.get("/events/" + id);
 }
 
 export const createEvento = (data) => {
-  return axios.post(baseUrl + "/events", data)
+  return axiosInstance.post("/events", data);
 }
 
 export const updateEvento = (id, data) => {
-  return axios.put(baseUrl + "/events/" + id, data)
+  return axiosInstance.put("/events/" + id, data);
 }
 
 export const deleteEvento = (id) => {
-  return axios.delete(baseUrl + "/events/" + id)
+  return axiosInstance.delete("/events/" + id);
 }
